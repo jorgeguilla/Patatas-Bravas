8bda997edf0eeb9895e96751ea83a8cfe6d23105
void pointer <jorgeguilla935@gmail.com>
2020-06-21 17:55:41 +0200

    2.3 Ejemplo añadido

--- 2/tema2-3.py
+++ 2/tema2-3.py
@@ -1,8 +1,15 @@
-def minimomaximo (lista):
-        minimo = maximo = lista [ 0 ]
-        for elem in lista:
-                if(elem > maximo):
-                        maximo = elem
-                elif (elem < minimo):
-                        minimo = elem
-        return [ minimo , maximo ]
+import random
+
+def minimomaximo(lista):
+    minimo = maximo = lista[0]
+    for elem in lista:
+        if elem > maximo:
+            maximo = elem
+        elif elem < minimo:
+            minimo = elem
+    return [minimo, maximo]
+
+
+ejemplo = [random.randint(0,100) for i in range(20)]
+print(ejemplo)
+print(minimomaximo(ejemplo))
