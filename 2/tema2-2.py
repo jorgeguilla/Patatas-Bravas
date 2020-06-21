c94e35d1864560cd0172d57b2592130e83391322
void pointer <jorgeguilla935@gmail.com>
2020-06-21 18:40:02 +0200

    2.2 y 2.6 código reducido

--- 2/tema2-2.py
+++ 2/tema2-2.py
@@ -1,16 +1,12 @@
 import random
 
+
 # Se trata de maximizar tasa/tamaño
 def ordenar(lista):
-    puntos = 0
-    valor = lambda elem: elem[0] / elem[1]
-    lista.sort(key=valor, reverse=True)
+    lista.sort(key=lambda elem: elem[0] / elem[1], reverse=True)
     return lista
 
 
-ejemplo = []
-for i in range(6):
-    ejemplo.append([random.randint(0, 100), random.randint(0, 100)])
-
+ejemplo = [[random.randint(0, 100), random.randint(1, 100)] for i in range(6)]
 print("Sean los ficheros ([tasa, tamaño]):", ejemplo)
 print("Su orden en el disco debería ser:", ordenar(ejemplo))
--- 2/tema2-6.py
+++ 2/tema2-6.py
@@ -1,3 +1,20 @@
-def shrek (longitudes):
-    longitudes.sort()
-    print ("El orden es ",longitudes)
+import random
+
+
+def shrek(longitudes):
+    suma = 0
+    while len(longitudes) != 1:
+        longitudes.sort()  # Toca ordenar a cada vuelta, los elementos cambian!
+        tramo = sum(longitudes[:2])
+        print(longitudes[:2], "=", tramo)
+        longitudes = longitudes[2:]
+        longitudes.append(tramo)
+        suma += tramo
+
+    print("El coste total es de", suma)
+
+
+ejemplo = [random.randint(1, 10) for i in range(8)]
+print("Tramos:", ejemplo)
+
+shrek(ejemplo)
