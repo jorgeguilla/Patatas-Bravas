c628db6fe36499deca2707e0ded461fa0b7664fc
void pointer <jorgeguilla935@gmail.com>
2020-06-21 13:46:13 +0200

    3.4 Ejemplo añadido

--- 3/tema3-4.py
+++ 3/tema3-4.py
@@ -1,26 +1,38 @@
-def	robot (botellas:list,tapones:list):
-        izdaT = [ ]
-        izdaB = [ ]
-        dchaT = [ ]
-        dchaB = [ ]
-        tapon = tapones [0]
-        botella = 0
-        for	i	in range ( len ( tapones )):
-	            if	botellas [ i ] == tapon :
-                        botella = botellas [ i ]
-        tapones.remove(tapon)
-        botellas.remove( botella )
+import random
 
-        for	i	in range ( len ( tapones )):
-                if	tapones [i] < botella:
-                        izdaT.append(tapones [i])
-                else:
-                        dchaT.append(tapones [i])
 
-                if	botellas [i] < tapon:
-                        izdaB.append(botellas [i])
-                else:
-                        dchaB.append( botellas [ i ])
+def robot(botellas: list, tapones: list):
+    izdaT, izdaB, dchaT, dchaB = [], [], [], []
+    tapon = tapones[0]
+    botella = 0
+    for i in range(len(tapones)):
+        if botellas[i] == tapon:
+            botella = botellas[i]
+            print(tapon, "con", botella)
+            break
+    tapones.remove(tapon)
+    botellas.remove(botella)
 
-        if ( len (izdaB)>0): robot (izdaB , izdaT)
-        if ( len (dchaB)>0): robot (dchaB , dchaT)
+    for i in range(len(tapones)):
+        if tapones[i] < botella:  # Dividimos tapones
+            izdaT.append(tapones[i])
+        else:
+            dchaT.append(tapones[i])
+
+        if botellas[i] < tapon:  # Dividimos botellas
+            izdaB.append(botellas[i])
+        else:
+            dchaB.append(botellas[i])
+
+    if len(izdaB) > 0:
+        robot(izdaB, izdaT)
+    if len(dchaB) > 0:
+        robot(dchaB, dchaT)
+
+
+tapones = [random.randint(0, 100) for i in range(70)]
+
+botellas = tapones.copy()
+botellas.sort(key = lambda l : random.randint(0,10)) #tapones desordenada
+a = 0
+robot(tapones, botellas)
